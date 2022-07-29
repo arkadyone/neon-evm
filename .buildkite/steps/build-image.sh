ac20423da3e82fd1f2eb265db6ec65ae827bea6c
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-07-29 19:14:48 +0400

    change solana version

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "Neon EVM revision=${BUILDKITE_COMMIT}"
 
-set ${SOLANA_REVISION:=v1.10.30}
+set ${SOLANA_REVISION:=v1.11.1-dumper-plugin}
 
 docker pull solanalabs/solana:${SOLANA_REVISION}
 echo "SOLANA_REVISION=$SOLANA_REVISION"
